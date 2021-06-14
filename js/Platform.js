83376e15916c0b090ca422b89d2bf150fcbc9559
peter <peter@crewpoints.com>
2021-06-14 17:46:42 -0400

    Initial Commit

---
+++ js/Platform.js
@@ -0,0 +1,15 @@
+﻿var tab1 = document.getElementById("tab1");
+
+tab1.addEventListener("click", () => {
+    document.getElementById("tabjobs").checked = true;
+})
+var tab2 = document.getElementById("tab2");
+
+tab2.addEventListener("click", () => {
+    document.getElementById("tabassets").checked = true;
+})
+var tab3 = document.getElementById("tab3");
+
+tab3.addEventListener("click", () => {
+    document.getElementById("tabpayments").checked = true;
+})
---
+++ js/mycrewpoints.js
@@ -0,0 +1,31 @@
+﻿var section1 = document.getElementsByClassName("section1");
+
+for (let i = 0; i < section1.length; i++) {
+    section1[i].addEventListener("click", () => {
+        document.getElementById("section1radio").checked = true;
+    })
+}
+
+var section2 = document.getElementsByClassName("section2");
+
+for (let i = 0; i < section1.length; i++) {
+    section2[i].addEventListener("click", () => {
+        document.getElementById("section2radio").checked = true;
+    })
+}
+
+var section3 = document.getElementsByClassName("section3");
+
+for (let i = 0; i < section1.length; i++) {
+    section3[i].addEventListener("click", () => {
+        document.getElementById("section3radio").checked = true;
+    })
+}
+
+var section4 = document.getElementsByClassName("section4");
+
+for (let i = 0; i < section1.length; i++) {
+    section4[i].addEventListener("click", () => {
+        document.getElementById("section4radio").checked = true;
+    })
+}
